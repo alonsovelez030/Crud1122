a517ab29f2632d16d63ad163590b7b6f9689e105
Alonso Velez <alonso_work@hotmail.com>
2018-08-30 09:56:50 -0500

    prueba de sentry en IONIC 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { UsersProvider } from '../providers/users/users';
 
+/* Sentry */
+import * as Sentry from 'sentry-cordova';
+Sentry.init({ dsn: 'https://0b527fa04ffd469ab78718f53b90906c@sentry.io/12718151271815' });
+
 /* SharedModule (Pipes, Directives, Components) */
 import { SharedModule } from '../shared/shared.module';
+import { SentryIonicErrorHandler } from '../providers/sentry-ionic-error-handler/sentry-ionic-error-handler';
 
 @NgModule({
   declarations: [
@@ -27,7 +32,8 @@ import { SharedModule } from '../shared/shared.module';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    // {provide: ErrorHandler, useClass: IonicErrorHandler},
+    {provide: ErrorHandler, useClass: SentryIonicErrorHandler},
     UsersProvider
   ]
 })
